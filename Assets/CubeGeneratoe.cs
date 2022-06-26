2a1395e8cb0cf8a8dc81ad2936f6d18065431258
chibuma555 <82083675+chibuma555@users.noreply.github.com>
2022-06-26 18:19:23 +0900

    Initial commit

---
+++ Assets/CubeGeneratoe.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeGeneratoe : MonoBehaviour
+{
+    public  GameObject anycube;
+    public List<GameObject> ListValue = new List<GameObject>();
+
+    void Start()
+    {
+        MakePrefab();
+    }
+
+    void MakePrefab()
+    { 
+        for (int i = 0; i < 7; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                Instantiate(anycube, new Vector3(i * 12.0f, 0.5f, j * 12.0f), Quaternion.identity);
+                ListValue.Add(anycube);
+            }
+        }
+    }
+
+    void Update()
+    {
+      
+    }
+}
---
+++ CubeMove.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CubeMove : MonoBehaviour
+{
+
+    //public float speed;
+    private Vector3 pos;
+    private bool isStop = false;
+    private float randomSpeed;
+
+    /*
+    private float timeReset = 5;
+    private float time = 0;
+    private bool flag;
+    private float moveBox;
+    */
+
+    void Start()
+    {
+
+        pos = this.gameObject.transform.position;
+        randomSpeed = Random.Range(1, 3f);
+        /*
+        StartCoroutine("CubeForward");
+        time = 0;
+        flag = true;
+        */
+    }
+
+   
+
+    void Update()
+    {
+        if (!isStop)
+        {
+            pos.y += Time.deltaTime * randomSpeed; // speedは移動速度
+            transform.position = pos;
+
+            if (pos.y > 3) // 終点（自由に変更可能）
+            {
+                isStop = true;
+            }
+        }
+        else
+        {
+            pos.y -= Time.deltaTime * randomSpeed;
+            transform.position = pos;
+
+            if (pos.y < -3) // 始点（自由に変更可能）
+            {
+                isStop = false;
+            }
+        }
+
+
+
+        /*
+        time += Time.deltaTime;
+
+        Vector3 p = new Vector3(0, moveBox, 0);
+        transform.Translate(p);
+
+        if (!flag)
+        {
+            if (time > timeReset)
+            {
+                StartCoroutine("CubeForward");
+                time = 0;
+                flag = true;
+            }
+        }
+
+        if (flag)
+        {
+            if (time > timeReset)
+            {
+                StartCoroutine("CubeBack");
+                time = 0;
+                flag = false;
+            }
+        }
+        */
+    }
+
+    /*
+    IEnumerator CubeForward()
+    {
+        yield return null;
+        moveBox = Random.Range(0f, 0.005f);
+        Debug.Log("達成");
+    }
+
+    IEnumerator CubeBack()
+    {
+        yield return null;
+        Random.InitState(40);
+        Debug.Log("歩達成");
+        moveBox *= -2;
+    }
+    */
+}
+
+
